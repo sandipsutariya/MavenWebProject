acfcc9265809160e23d890926b38071fde304a2e
Gauri <gauprabhu@gmail.com>
2016-05-05 14:17:20 +0530

    selenium Changes

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -16,7 +16,7 @@
 			<div class="login-options">Choose one of the following sign up methods.</div>
 			<a class="google" href="#">Google+</a>
 			<a class="twitter" href="#">Twitter</a>
-			<a class="facebook" href="#">Facebook</a>
+			<!-- <a class="facebook" href="#">Facebook</a> -->
 			<div class="clr">
 				<hr />
 			
@@ -28,7 +28,7 @@
 			<div class="forms">
 				<form action="" method="get" name="register">
 					<input name="fullname" type="text" placeholder="Enter your full name..." size="70" onClick="border: 1px solid #30a8da;" id="username" /> 
-					<input name="email" type="text" placeholder="Enter your email address..." size="70" onClick="border: 1px solid #30a8da;" id="mail" /> 
+					<input name="email" type="text" placeholder="Enter your email address..." size="70" onClick="border: 1px solid #30a8da;" id="mail" />
 					<input name="password" type="password" placeholder="Enter a password..." size="70" onClick="border: 1px solid #30a8da;" id="password" />
 				</form>
 			</div>
---
+++ src/test/java/testng/TestNG1.java
@@ -0,0 +1,63 @@
+package testng;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class TestNG1 {
+  private WebDriver driver;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeMethod
+@BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+	  System.setProperty("webdriver.ie.driver", "C:\\gauri\\Personal\\Software\\IEDriverServer.exe");
+    driver = new InternetExplorerDriver();
+    
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testSampleLoginApplication() throws Exception {
+    // ERROR: Caught exception [ERROR: Unsupported command [openWindow | file:///C:/Users/gauri.devdas.prabhu/MavenWebProject/src/main/webapp/index.jsp | ]]
+	  driver.get("http://localhost:8080/firstmavenwebapp/index.jsp");
+    AssertJUnit.assertTrue(isElementPresent(By.linkText("Facebook")));
+    AssertJUnit.assertTrue(isElementPresent(By.linkText("Twitter")));
+    AssertJUnit.assertTrue(isElementPresent(By.linkText("Google+")));
+    AssertJUnit.assertEquals(driver.getTitle(), "Sample Login Form");
+    driver.findElement(By.id("username")).click();
+    AssertJUnit.assertTrue(isElementPresent(By.id("username")));
+    AssertJUnit.assertTrue(isElementPresent(By.id("mail")));
+    AssertJUnit.assertTrue(isElementPresent(By.id("password")));
+    driver.findElement(By.linkText("Create My Account")).click();
+  }
+
+  @AfterMethod
+@AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      Assert.fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+}
